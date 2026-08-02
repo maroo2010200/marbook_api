b5fa80cfd4d8a7871c8464e9901982a912706f87
maroo2010200 <maroo2010200@gmail.com>
2026-08-02 23:03:24 +0300

    Register User entity in DbContext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,3 +1,4 @@
+using MarbookApi.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace MarbookApi.Data
@@ -6,6 +7,46 @@ namespace MarbookApi.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
+        }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.HasPostgresExtension("citext");
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(u => u.Id);
+
+                entity.Property(u => u.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+                
+                entity.Property(u => u.Username)
+                    .HasColumnType("citext");
+
+                entity.HasIndex(u => u.Username)
+                    .IsUnique();
+        
+                entity.Property(u => u.Email).
+                    HasColumnType("citext")
+                    .IsRequired();
+
+                entity.HasIndex(u => u.Email)
+                    .IsUnique();
+                
+                entity.Property(u => u.Password)
+                    .IsRequired();
+                
+                entity.Property(u => u.Birthdate)
+                    .IsRequired();
+                
+                entity.Property(u => u.CreatedAt)
+                    .HasDefaultValueSql("NOW()");
+                entity.Property(u => u.UpdatedAt)
+                    .HasDefaultValueSql("NOW()");
+            });
         }
     }
 }
