9d16291f7034569fdc2a1f9ac85ce73ab57d350e
maroo2010200 <maroo2010200@gmail.com>
2026-08-10 21:41:23 +0300

    Register JwtOptions into Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
+using MarbookApi;
 using MarbookApi.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.Configure<JwtOptions>(
+    builder.Configuration.GetSection("Jwt")
+);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddDbContext<AppDbContext>(options =>
