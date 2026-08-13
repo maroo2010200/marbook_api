a8b8adaa524aee83d900bafd40b51a21050f59c1
maroo2010200 <maroo2010200@gmail.com>
2026-08-13 16:35:07 +0300

    Add JWT Bearer authentication in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 using MarbookApi;
 using MarbookApi.Data;
 using MarbookApi.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +12,25 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.Configure<JwtOptions>(
     builder.Configuration.GetSection("Jwt")
 );
+
+var jwtOptions = builder.Configuration.GetSection("Jwt").Get<JwtOptions>()!;
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+   options.TokenValidationParameters = new TokenValidationParameters
+   {
+       ValidateIssuer = true,
+       ValidIssuer = jwtOptions.Issuer,
+
+       ValidateAudience = true,
+       ValidAudience = jwtOptions.Audience,
+
+       ValidateLifetime = true,
+
+       ValidateIssuerSigningKey = true,
+       IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.SigningKey))
+   };
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -31,6 +53,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
