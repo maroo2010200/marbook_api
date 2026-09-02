1c01efe7633de5e8255b0c8ad9b906dfb378a61e
maroo2010200 <maroo2010200@gmail.com>
2026-09-02 16:14:21 +0300

    CREATE migration (search indexes (pg_trgm + FTS GIN) for posts)

---
+++ Migrations/20260902122749_AddSearchIndexes.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MarbookApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSearchIndexes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Enable pg_trgm extension for PostgreSQL to support trigram indexes (safe to run multiple times)
+            migrationBuilder.Sql("CREATE EXTENSION IF NOT EXISTS pg_trgm;");
+
+            // Gin trigram table for ILIKE substring search
+            migrationBuilder.Sql(@"
+                CREATE INDEX IF NOT EXISTS IX_Posts_Content_Trgm 
+                ON ""Posts"" USING GIN (""Content"" gin_trgm_ops);
+            ");
+
+            // Gin index for Full Text Search
+            migrationBuilder.Sql(@"
+                CREATE INDEX IF NOT EXISTS IX_Posts_Content_FTS 
+                ON ""Posts"" USING GIN (to_tsvector('english', ""Content""));
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP INDEX IF EXISTS IX_Posts_Content_Trgm;");
+            migrationBuilder.Sql("DROP INDEX IF EXISTS IX_Posts_Content_FTS;");
+        }
+    }
+}
