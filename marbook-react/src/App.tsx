b1c5dc951b83e1fcc2d378925c3cd173c4b43a33
maroo2010200 <maroo2010200@gmail.com>
2026-09-17 06:24:40 +0300

    feat: add login form with controlled inputs and onSubmit

--- marbook-react/src/App.tsx
+++ marbook-react/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
@@ -8,6 +9,10 @@ function App() {
         <Route
           path="/"
           element={<HomePage />}
+        />
+        <Route
+          path="/login"
+          element={<LoginPage/>}
         />
       </Routes>
     </BrowserRouter>
